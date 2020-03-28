bffbe39dec1446a0ca5875a9f6c84903640e9c16
Peter Ho <pho@buhao.cc>
2020-03-28 19:55:07 -0400

    Added menu and mash

--- GC_Controller.ino
+++ GC_Controller.ino
@@ -1,139 +1,50 @@
-/*
-  Copyright (c) 2014-2016 NicoHood
-  See the readme for credit to other people.
+#include "menu.hh"
+#include <Nintendo.h>
 
-  GamecubeController example
-  Enter any key into the serial monitor to start reading.
-  Press A on the controller to enable rumble.
-*/
+CGamecubeConsole console(8);
+CGamecubeController controller(7);
+Menu menu(&controller);
 
-#include "Nintendo.h"
-
-// Define a Gamecube Controller
-CGamecubeController GamecubeController(7);
-
-// Pin definitions
-#define pinLed LED_BUILTIN
+static const Gamecube_Report_t empty;
 
 void setup()
 {
-  // Set up debug led
-  pinMode(pinLed, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  // Start debug serial
-  while (!Serial);
-  Serial.begin(115200);
+    menu.set_item(Menu::dpad_down, Menu::A, mash);
 
-  // Wait for some Serial input
-  Serial.println(F("Enter any key to start reading the controller."));
-  Serial.println(F("Press A on the controller to enable rumble."));
-  while (Serial.read() == -1);
+    while (!Serial);
+    Serial.begin(115200);
 
-  Serial.println(F("Starting controller emulation"));
-  Serial.println();
+    Serial.println(F("Press any key."));
+    while (Serial.read() == -1);
+
+    Serial.println(F("Starting"));
+    Serial.println();
+
+    controller.read();
 }
 
 void loop()
 {
-  // Try to read the controller data
-  if (GamecubeController.read())
-  {
-    // Print Controller information
-    auto status = GamecubeController.getStatus();
-    auto report = GamecubeController.getReport();
-    print_gc_report(report, status);
-    delay(100);
-
-    // Rumble if button "A" was pressed
-    if (report.a) {
-      GamecubeController.setRumble(true);
-    }
-    else {
-      GamecubeController.setRumble(false);
-    }
-  }
-  else
-  {
-    // Add debounce if reading failed
-    Serial.println(F("Error reading Gamecube controller."));
-    digitalWrite(pinLed, HIGH);
-    delay(1000);
-  }
-  digitalWrite(pinLed, LOW);
+    menu.loop();
 }
 
-void print_gc_report(Gamecube_Report_t &gc_report, Gamecube_Status_t &gc_status)
+
+void mash()
 {
-  // Print device information
-  Serial.print(F("Device: "));
-  switch (gc_status.device) {
-    case NINTENDO_DEVICE_GC_NONE:
-      Serial.println(F("No Gamecube Controller found!"));
-      break;
-    case NINTENDO_DEVICE_GC_WIRED:
-      Serial.println(F("Original Nintendo Gamecube Controller"));
-      break;
-
-    default:
-      Serial.print(F("Unknown "));
-      Serial.println(gc_status.device, HEX);
-      break;
-  }
-
-  // Print rumble state
-  Serial.print(F("Rumble "));
-  if (gc_status.rumble)
-    Serial.println(F("on"));
-  else
-    Serial.println(F("off"));
-
-  // Prints the raw data from the controller
-  Serial.println();
-  Serial.println(F("Printing Gamecube controller report:"));
-  Serial.print(F("Start:\t"));
-  Serial.println(gc_report.start);
-
-  Serial.print(F("Y:\t"));
-  Serial.println(gc_report.y);
-
-  Serial.print(F("X:\t"));
-  Serial.println(gc_report.x);
-
-  Serial.print(F("B:\t"));
-  Serial.println(gc_report.b);
-
-  Serial.print(F("A:\t"));
-  Serial.println(gc_report.a);
-
-  Serial.print(F("L:\t"));
-  Serial.println(gc_report.l);
-  Serial.print(F("R:\t"));
-  Serial.println(gc_report.r);
-  Serial.print(F("Z:\t"));
-  Serial.println(gc_report.z);
-
-  Serial.print(F("Dup:\t"));
-  Serial.println(gc_report.dup);
-  Serial.print(F("Ddown:\t"));
-  Serial.println(gc_report.ddown);
-  Serial.print(F("Dright:\t"));
-  Serial.println(gc_report.dright);
-  Serial.print(F("Dleft:\t"));
-  Serial.println(gc_report.dleft);
-
-  Serial.print(F("xAxis:\t"));
-  Serial.println(gc_report.xAxis, DEC);
-  Serial.print(F("yAxis:\t"));
-  Serial.println(gc_report.yAxis, DEC);
-
-  Serial.print(F("cxAxis:\t"));
-  Serial.println(gc_report.cxAxis, DEC);
-  Serial.print(F("cyAxis:\t"));
-  Serial.println(gc_report.cyAxis, DEC);
-
-  Serial.print(F("L:\t"));
-  Serial.println(gc_report.left, DEC);
-  Serial.print(F("R:\t"));
-  Serial.println(gc_report.right, DEC);
-  Serial.println();
+    int x, y;
+    int led = 0;
+    float deg = 0;
+    Gamecube_Report_t re;
+    do {
+        x = 255 * cos(deg);
+        y = 255 * sin(deg);
+        re = empty;
+        re.x = x;
+        re.y = y;
+        console.write(re);
+        deg += 0.1; // Quarter-circle every ~16 steps
+        digitalWrite(LED_BUILTIN, led = !led);
+    } while (!controller.getReport().start);
 }
---
+++ menu.hh
@@ -0,0 +1,64 @@
+#pragma once
+// #include <unordered_map>
+#include <Nintendo.h>
+
+class Menu {
+private:
+    CGamecubeController *controller;
+    typedef void (*GC_func)(void);
+    // std::unordered_map<int8_t, GC_func> map;
+
+    GC_func map[sizeof(uint8_t)] = {nullptr};
+public:
+    Menu(CGamecubeController *controller);
+    void set_item(uint8_t dpad, uint8_t button, GC_func f);
+    void loop();
+
+    static const int dpad_down;
+    static const int dpad_left;
+    static const int dpad_right;
+    static const int dpad_up;
+
+    static const int A;
+    static const int B;
+    static const int X;
+    static const int Y;
+};
+
+const int Menu::dpad_left  = 1 << 0;
+const int Menu::dpad_right = 1 << 1;
+const int Menu::dpad_down  = 1 << 2;
+const int Menu::dpad_up    = 1 << 3;
+
+const int Menu::A = 1 << 0;
+const int Menu::B = 1 << 1;
+const int Menu::X = 1 << 2;
+const int Menu::Y = 1 << 3;
+
+Menu::Menu(CGamecubeController *c)
+{
+    controller = c;
+}
+
+void Menu::set_item(uint8_t dpad, uint8_t button, GC_func f)
+{
+    int8_t command = dpad << 4 | button;
+    map[command] = f;
+}
+
+void Menu::loop()
+{
+    uint8_t dpad, button;
+    uint16_t command;
+    Gamecube_Report_t re = controller->getReport();
+
+    dpad = re.dleft | re.dright | re.ddown | re.dup;
+    button = re.a | re.b | re.x | re.y;
+
+    command = dpad << 4 | button;
+
+    if (button == 0)
+        return;
+
+    map[command]();
+}
