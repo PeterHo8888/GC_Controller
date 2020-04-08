825b98f21fe0d842a0e85070b05ee515b148aabd
Peter Ho <pho@buhao.cc>
2020-04-08 01:27:47 -0400

    remove sdi, fix mash, optimize crouch cancel taunt, add no-smoke taunt, add diddy infinite

--- GC_Controller.ino
+++ GC_Controller.ino
@@ -13,12 +13,13 @@ void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
 
-    menu.set_item(Menu::dpad_up, Menu::A, mash);
+    menu.set_item(Menu::dpad_left, Menu::B, mash);
     menu.set_item(Menu::dpad_down, Menu::B,
         crouch_cancelled_walk_cancelled_turnaround_cancelled_crouch);
-    menu.set_item(Menu::dpad_right, Menu::A, online_taunt);
-    menu.set_item(Menu::dpad_left, Menu::A, SDI);
-    menu.set_item(Menu::dpad_up, Menu::B, salty_rage_quit);
+    menu.set_item(Menu::dpad_up, Menu::B, online_taunt);
+    menu.set_item(Menu::dpad_right, Menu::B, salty_rage_quit);
+    
+    menu.set_item(Menu::dpad_down, Menu::A, diddy_infinite);
 
     while (!Serial);
     Serial.begin(115200);
@@ -36,12 +37,17 @@ void loop()
     menu.loop();
 }
 
+inline bool cancel(Gamecube_Report_t re)
+{
+    return re.left < 128 && re.right < 128 && !re.x && !re.y;
+}
+
 void mash()
 {
     Gamecube_Report_t re;
     int x, y;
     float deg = 0;
-    //digitalWrite(LED_BUILTIN, 1);
+    digitalWrite(LED_BUILTIN, 1);
     do {
         x = 127 * cos(deg) + 127;
         y = 127 * sin(deg) + 127;
@@ -49,57 +55,49 @@ void mash()
         re.xAxis = x;
         re.yAxis = y;
         console.write(re);
-        deg += 0.1; // Quarter-circle every ~16 steps
+        deg += 90; // Quarter-circle every ~16 steps
+        if (deg > 360)
+            deg = 0;
+        controller.read();
         re = controller.getReport();
-        delay(step);
-        digitalWrite(LED_BUILTIN, 0);
-    } while (!re.start);
+    } while (cancel(re));
 
+    digitalWrite(LED_BUILTIN, 0);
 }
 
 void crouch_cancelled_walk_cancelled_turnaround_cancelled_crouch()
 {
     Gamecube_Report_t re;
-    int frames = 0;
-    float deg = 0;
+    int frame = 0;
+    int dir = 1;
     
     const int crouch = 15;
     const int turn = 3;
     digitalWrite(LED_BUILTIN, 1);
     do {
         re = empty;
-        re.cxAxis = 128;
-        re.cyAxis = 128;
         
-        frames++;
-        if (frames >= 0 && frames < crouch) {
+        ++frame;
+        if (frame >= 0 && frame < crouch) {
             re.xAxis = 128; // go down
             re.yAxis = 0;
-        } else if (frames >= crouch && frames < crouch+turn) {
-            re.xAxis = 128-40; //go left
+        } else if (frame < crouch+turn) {
+            re.xAxis = 128-40*dir; //go left
             re.yAxis = 128;
-        } else if (frames >= crouch+turn && frames < crouch+2*turn) {
-            re.xAxis = 128+40; //go right
-            re.yAxis = 128;
-        } else if (frames >= crouch+2*turn && frames < 2*crouch+2*turn) {
-            re.xAxis = 128; // go down
-            re.yAxis = 0;
-        } else if (frames >= 2*crouch+2*turn && frames < 2*crouch+3*turn) {
-            re.xAxis = 128+40; //go right
-            re.yAxis = 128;
-        } else if (frames >= 2*crouch+3*turn && frames < 2*crouch+4*turn) {
-            re.xAxis = 128-40; //go left
+        } else if (frame < crouch+2*turn) {
+            re.xAxis = 128+40*dir; //go right
             re.yAxis = 128;
         } else {
             re.xAxis = 128; // go down
             re.yAxis = 0;
-            frames = 0;
+            frame = 0;
+            dir = -dir;
         }
         
         console.write(re); 
         controller.read();
         re = controller.getReport();
-    } while (!re.a);
+    } while (cancel(re));
 
     digitalWrite(LED_BUILTIN, 0);
 }
@@ -110,41 +108,23 @@ void online_taunt()
     int frame = 0;
     digitalWrite(LED_BUILTIN, 1);
     do {
-        re.yAxis = 127;
-
-        // Can be 2-5 frames with smoke, 1 without
-        if (frame << 1 == 0)
-            re.xAxis = 0;
-        else if (frame << 1 == 1)
+        re = empty;
+        ++frame;
+        if (frame < 6) {
             re.xAxis = 255;
-
-        console.write(re);
-
-        if (++frame == 4)
+            re.yAxis = 128;
+        }
+        else if (frame < 10) {
+            re.xAxis = 0;
+            re.yAxis = 128;
+        } else {
             frame = 0;
-
-        re = controller.getReport();
-        delay(step);
-    } while (!re.start);
-
-    digitalWrite(LED_BUILTIN, 0);
-}
-
-void SDI()
-{
-    int frame = 0;
-    Gamecube_Report_t re;
-    digitalWrite(LED_BUILTIN, 1);
-    do {
-        re = controller.getReport();
-        if(frame && 1 == 1){
-            re.xAxis = 127;
-            re.yAxis = 127;
         }
         console.write(re);
-        frame++;
-        delay(step);
-    } while(!re.start);
+        controller.read();
+        re = controller.getReport();
+    } while (cancel(re));
+
     digitalWrite(LED_BUILTIN, 0);
 }
 
@@ -172,4 +152,41 @@ void salty_rage_quit()
     console.write(re);
     delay(step);
     digitalWrite(LED_BUILTIN, 0);
+}
+
+void diddy_infinite()
+{
+    Gamecube_Report_t re;
+    digitalWrite(LED_BUILTIN, 1);
+    
+    int frame = 0;
+    int dir = 1;
+    do {
+        re = empty;
+        if (frame < 10) {
+            re.b = 1;
+            re.yAxis = 0;
+            re.xAxis = 128 - 10*dir;
+        } else if (frame < 110) {
+            re.y = 1;
+            re.xAxis = 128 + 50*dir;
+        } else if (frame < 120) {
+            re.z = 1;
+            re.xAxis = 128 + 50*dir;
+        } else if (frame < 170) {
+            re.cyAxis = 0;
+            re.xAxis = 128 + 50*dir;
+        } else if (frame < 180) {
+            re.yAxis = 0;
+        } else {
+            frame = 0;
+            dir = -dir;
+        }
+        ++frame;
+        console.write(re);
+        controller.read();
+        re = controller.getReport();
+    } while (cancel(re));
+        
+    
 }
